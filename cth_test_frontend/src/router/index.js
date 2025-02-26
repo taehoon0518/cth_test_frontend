234a3a3e9af6a79e47cccf6259430ebfb2a8e6b2
taehoon0518 <cjsxogns0518@gmail.com>
2025-02-26 14:35:46 +0900

    [Page] 게시물 상세 페이지

--- cth_test_frontend/src/router/index.js
+++ cth_test_frontend/src/router/index.js
@@ -2,11 +2,13 @@ import { createWebHistory, createRouter } from 'vue-router'
 
 import BoardListPage from '../components/BoardListPage.vue'
 import BoardRegisterPage from '../components/BoardRegisterPage.vue';
+import BoardDetailPage from '../components/BoardDetailPage.vue';
 
 const routes = [
     { path: '/', redirect: '/board/list' }, 
     { path: '/board/list', component: BoardListPage },
     { path: '/board/register', component: BoardRegisterPage },
+    { path: '/board/:idx', component: BoardDetailPage },
 
 ]
 
